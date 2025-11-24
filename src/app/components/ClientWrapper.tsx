46f9575c42665b83f89b37bb18915917ff5b1739
Obi Habibi <obi6745@gmail.com>
2025-11-23 20:52:26 -0500

    start components and continue add more to the folder

---
+++ src/app/components/ClientWrapper.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { AppProvider } from '@/context/AppContext'
+import Navbar from '@/components/Navbar'
+
+export default function ClientWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <AppProvider>
+      <Navbar />
+      <main className="min-h-screen">
+        {children}
+      </main>
+    </AppProvider>
+  )
+}
